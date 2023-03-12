79d7feabce3eca4a8a02bb24e4c4399f31171330
thiendang201 <thiendang201@gmail.com>
2023-03-12 20:32:07 +0700

    remove fullscreen

--- features/scan/components/WebCam.tsx
+++ features/scan/components/WebCam.tsx
@@ -1,19 +1,16 @@
 import CircleButton from '@/common/components/Button/CircleButton';
 import { useWindowSize } from '@/common/hooks/useWindowSize';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Webcam from 'react-webcam';
 import Image from 'next/image';
 import { useScreenHeight } from '@/common/hooks/useScreenHeight';
 import { FacingMode } from '@/common/enums/common';
-import { useMobile } from '@/common/hooks/useMobile';
 
 export default function WebCam() {
-  const isMobile = useMobile();
   const heightVariant = useScreenHeight();
   const { width, height } = useWindowSize();
   const [imagePreview, setImagePreview] = useState('');
   const webcamRef = useRef<Webcam>(null);
-  const fullScreenRef = useRef<HTMLDivElement>(null);
   const videoConstraints = {
     facingMode: FacingMode.environment,
   };
@@ -23,23 +20,14 @@ export default function WebCam() {
     setImagePreview(imageSrc ?? '');
   }, [webcamRef]);
 
-  console.log('webcam');
-
-  useEffect(() => {
-    fullScreenRef.current?.requestFullscreen().catch((e) => {
-      console.log(e);
-    });
-    // console.log(navigator.userAgent, videoConstraints);
-  }, []);
-
   return (
-    <div ref={fullScreenRef} style={{ height: `var(${heightVariant})` }} className="relative">
+    <div style={{ height: `var(${heightVariant})` }} className="relative">
       <Webcam
         audio={false}
         ref={webcamRef}
         screenshotFormat="image/jpeg"
         imageSmoothing
-        mirrored={isMobile}
+        mirrored={videoConstraints.facingMode === FacingMode.user}
         videoConstraints={videoConstraints}
         screenshotQuality={1}
         style={{
